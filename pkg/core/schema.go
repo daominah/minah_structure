371a9e41abed3c139da89bdb8a23b46c70816070
Đào Thanh Tùng <tung.dao@difisoft.com>
2022-11-10 08:36:21 +0700

    add BSD2 license

--- pkg/core/schema.go
+++ pkg/core/schema.go
@@ -1,3 +1,9 @@
 // Package core contains business logic. Can be tested without external
 // resources (database, HTTP, websocket, message queue, file, ..)
 package core
+
+// Example is an example :v
+type Example struct {
+	// F1 hohohaha
+	F1 float64
+}
